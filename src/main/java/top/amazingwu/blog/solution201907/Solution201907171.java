b844b2f20f0268fe3e0054842922deba7bf24e80
Amazingwu <amazingjadewu@gmail.com>
2020-03-23 22:45:39 +0800

    两数的总和2

--- src/main/java/top/amazingwu/blog/solution201907/Solution201907171.java
+++ src/main/java/top/amazingwu/blog/solution201907/Solution201907171.java
@@ -16,10 +16,13 @@ public class Solution201907171 {
         LinkedList<List<Integer>> result = new LinkedList<>();
         if (candidates == null || candidates.length == 0) return result;
         Arrays.sort(candidates);
+        // indexStack存储当前操作的数的序号
         LinkedList<Integer> indexStack = new LinkedList<>();
         indexStack.push(-1);
+        // value存储当前待构成target的数据集合
         LinkedList<Integer> value = new LinkedList<>();
         int sum = 0;
+        // 开始回溯遍历
         while (!indexStack.isEmpty()) {
             Integer index = indexStack.pop();
             if (index != -1) {
@@ -27,9 +30,11 @@ public class Solution201907171 {
                 value.removeLast();
             }
             if (index < candidates.length - 1) {
+                // 当sum<=target时，不断尝试+当前数，直到符合或超出,再尝试下一个数
                 while (sum <= target) {
                     value.addLast(candidates[index + 1]);
                     indexStack.push(index + 1);
+                    // 符合后记录下相关组合情况
                     if (candidates[index + 1] + sum == target) {
                         result.add(value);
                         value = new LinkedList<>();
@@ -40,6 +45,7 @@ public class Solution201907171 {
                         sum += candidates[index + 1];
                     }
                 }
+                // 清除栈顶的数，尝试下一个
                 sum -= candidates[index + 1];
                 indexStack.pop();
                 value.removeLast();
